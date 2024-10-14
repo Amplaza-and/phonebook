81e389ce39260e96127ad08b9b9ccc11088b5218
Amplaza-and <albertomplazaperez@gmail.com>
2024-10-14 19:17:30 +0200

    Update person.js

--- models/person.js
+++ models/person.js
@@ -6,10 +6,22 @@ console.log('connecting to', url)
 mongoose.connect(url)
     .then(result => {    console.log('connected to MongoDB')  })  .catch(error => {    console.log('error connecting to MongoDB:', error.message)  })
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {type: String,    minLength: 3,    required: true},
+    number: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d+/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`,
+        required: true,
+        unique: true
+      },
+    }
   })
-  
+
+  // Validation succeeds! Phone number is defined
+  // and fits `DDD-DDD-DDDD`
   personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
